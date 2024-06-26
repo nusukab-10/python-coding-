8e310d8d3849d6eb1c088dad9c5b663f7025f2e4
nusukab-10 <nusukab11508@gmail.com>
2024-06-26 14:35:07 +0600

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,16 @@
+#My first code to print Hello World!
+print("Hello World!")
+
+# Here input() defines user input which set by the user.
+a= int(input("Enter any value:"))
+b= int(input("Enter any value:"))
+
+Sum = a+b
+Sub= a-b
+Mul = a*b
+Div = a/b
+
+print("the sum value is:",Sum)
+print("the subtract value is:",Sub)
+print("the multiply value is:",Mul)
+print("the division value is:",Div)
